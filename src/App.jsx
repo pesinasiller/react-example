af34318a669bde4356371f80406a1da904795fab
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2025-10-10 17:18:58 +0400

    button style

--- src/App.jsx
+++ src/App.jsx
@@ -37,16 +37,21 @@ class App extends Component {
   }*/
 
   render(){
+
+    
     const usersList = users.map((user)=> {
       const { name, country, profilePicture } = user; 
       return (<UserInfo name={name}  country={country} image={profilePicture} />)
     });
+
+
     return(
-      <div>
+      <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        {/* 
         <UserInfoClass name="Carlos" country="Mexico" color="orange" />
         <UserInfo name="Mahdia" country="Afganistan" color="blue" image="https://cdn2.thecatapi.com/images/5dF28H7__.jpg" />
         <UserInfo name="Maria" country="España" color="pink" image="https://cdn2.thecatapi.com/images/5dF28H7__.jpg"  />
-        <button>Hello world asdfsadf</button>
+         */}
         {usersList}
       </div>
     )
--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import ProfilePicture from "./ProfilePicture";
+import './UserInfo.css';
 
 function UserInfo(props) {
     const [counter, setCounter] = useState(0);
     const { name, country, color, image } = props;
+    const [pressed, setPressed] = useState(false);
+
+    const togglePressed = () => {
+      setPressed(!pressed)
+    }
+
+    const buttonStyle = pressed ?  "user-info-pressed" : "user-info";
 
   return (
-    <div style={{ border: "1px solid black" , backgroundColor: color }}>
+    <div onClick={togglePressed} togglePressed className={buttonStyle} style={{ border: "1px solid black" , backgroundColor: color }}>
         <ProfilePicture url={image}  width="300" />
         <h1>{name}</h1>
         <h2>{country}</h2>
