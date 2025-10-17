7db8e82e3b813e4046d4022d0a86c49c63c25283
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2025-10-17 15:57:05 +0400

    calls endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,150 @@
 import React, { Component } from "react";
 import UserInfo from "./UserInfo";
-import UserInfoClass from './UserInfoClass';
-
+import UserInfoClass from "./UserInfoClass";
 
 const users = [
-  { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-  { name: "carlos 1", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos 2", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos 3", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
+  {
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos 1",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos 2",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos 3",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
 ];
-
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+    };
   }
-  /*
-  componentDidMount(){
-     fetch("http://127.0.0.1:3000/users")
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:3000/users")
       .then((res) => res.json())
-      .then((data)=>{
-        console.log({data})
-      })
-  
-  }*/
+      .then((data) => {
+        console.log({ data });
 
-  render(){
+        this.setState({ users: data });
+      });
+  }
 
-    
-    const usersList = users.map((user)=> {
-      const { name, country, profilePicture } = user; 
-      return (<UserInfo name={name}  country={country} image={profilePicture} />)
+  render() {
+    console.log("Rendering App component");
+    console.log(this.state.users);
+
+    const usersList = this.state.users.map((user) => {
+      const { name, country, profilePicture } = user;
+      return <UserInfo name={name} country={country} image={profilePicture} />;
     });
-
-
-    return(
-      <div style={{display: 'flex', flexWrap: 'wrap'}}>
+    console.log({ usersList });
+    return (
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {/* 
         <UserInfoClass name="Carlos" country="Mexico" color="orange" />
         <UserInfo name="Mahdia" country="Afganistan" color="blue" image="https://cdn2.thecatapi.com/images/5dF28H7__.jpg" />
@@ -54,12 +152,11 @@ class App extends Component {
          */}
         {usersList}
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
 
 /*
 function App() {
